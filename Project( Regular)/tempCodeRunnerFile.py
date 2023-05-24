d94e62e256df75897419924d02c983c6d68d0829
Rafi Sharkar <rafisharkar144@gmail.com>
2023-05-24 14:14:14 +0600

    first python commit added

---
+++ Project( Regular)/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+L_books = ["a","b","d","e","c","a","b","c","a","x","c"]
+# L_books= [1,5,1,4,6,9,7,4,6,4,57,6,4,3,1,45,4,5]
+
+L_books_set = set(L_books)
+L_books_set_L=list(L_books_set)
+print(L_books_set_L)
